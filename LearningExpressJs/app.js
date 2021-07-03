54abea8a261d5b93231a818e0b9fe2fdc5ac931d
AhmedMohammed3 <ahm.moh.has@gmail.com>
2021-07-03 10:40:51 +0200

    Finished Lesson 10 in Module 5

--- LearningExpressJs/app.js
+++ LearningExpressJs/app.js
@@ -1,5 +1,29 @@
-const http = require('http');
+//==============Core Modules========
+//==============Third Party=========
+const express = require("express");
+const bodyParser = require("body-parser");
+//==============My Code Files=======
+//==================================
+const app = express();
 
-const server = http.createServer();
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-server.listen(1234);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Second Page</h1>");
+});
+app.listen(1234);
